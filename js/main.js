ca2d504d30965dae56585d7694c4681304f110c1
Logan Lyons <nospecdev@gmail.com>
2025-04-09 22:47:13 -0500

    Added functionality for randomizing recipes based on a main ingredient

--- js/main.js
+++ js/main.js
@@ -18,20 +18,41 @@
 // *** IGNORE - USING AS REFERENCE FROM PAST PROJECT *** ///
 
 
-document.querySelector('getRecipeBtn').addEventListener('click', getRecipe)
+document.querySelector('#getRecipeBtn').addEventListener('click', getRecipe)
 
 function getRecipe() {
   const ingredients = document.querySelector('#ingredients').value.trim()
 
   if (!ingredients) {
     alert('Please enter some ingredients.')
+    return
   }
-}
 
-const url = ``
 
-fetch(url)
+const searchUrl = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${encodeURIComponent(ingredients)}`
+
+fetch(searchUrl)
   .then(res => res.json())
   .then(data => {
+    const meals = data.meals;
 
+    const randomMeal = meals[Math.floor(Math.random() * meals.length)];
+    const mealId = randomMeal.idMeal;
+    
+    const detailUrl = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
+    return fetch(detailUrl)
   })
+
+  .then(res => res.json())
+  .then(data => {
+    const meal = data.meals[0]
+    displayRecipe(meal)
+  })
+}
+
+function displayRecipe(meal) {
+  document.querySelector('#recipeTitle').innerText = meal.strMeal;
+  document.querySelector('#recipeImage').src = meal.strMealThumb;
+  document.querySelector('#recipeInstructions').innerText = meal.strInstructions;
+  document.querySelector('#recipeContainer').style.display = 'block';
+}
