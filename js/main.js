cd018b43c9b18ec0a134bcbb1f7bba6e893512ea
nospecdev <nospecdev@gmail.com>
2025-04-10 21:00:33 -0500

    Fixed some responsiveness issues

--- js/main.js
+++ js/main.js
@@ -34,6 +34,7 @@ const searchUrl = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${encode
 fetch(searchUrl)
   .then(res => res.json())
   .then(data => {
+    console.log(data)
     const meals = data.meals;
 
     const randomMeal = meals[Math.floor(Math.random() * meals.length)];
@@ -45,14 +46,26 @@ fetch(searchUrl)
 
   .then(res => res.json())
   .then(data => {
+    console.log(data)
     const meal = data.meals[0]
     displayRecipe(meal)
   })
 }
 
 function displayRecipe(meal) {
+  console.log('getRecipe() was called')
   document.querySelector('#recipeTitle').innerText = meal.strMeal;
   document.querySelector('#recipeImage').src = meal.strMealThumb;
   document.querySelector('#recipeInstructions').innerText = meal.strInstructions;
   document.querySelector('#recipeContainer').style.display = 'block';
+
+  document.querySelector('#recipeIngredients').innerText = ''
+
+  for( let i = 1; i <=20; i++ ) {
+    if (meal[`strIngredient${i}`] && meal[`strIngredient${i}`].trim() !== '') {
+      const li = document.createElement('li')
+      li.textContent = `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+      document.querySelector('#recipeIngredients').appendChild(li)
+    }
+  }
 }
